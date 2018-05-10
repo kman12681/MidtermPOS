bfb1ecb8c6c70b5d56d3154f9a7ce3362192da6e
kman12681 <keith.rigling@gmail.com>
2018-05-10 16:26:06 -0400

    program cs cleaned up

--- MidtermPOS/Program.cs
+++ MidtermPOS/Program.cs
@@ -14,39 +14,39 @@ namespace MidtermPOS
             Console.WriteLine("Welcome to the ***");
 
             ChooseProduct();
-            ViewFullMenu();
-            //ShowInventory();
+            ViewFullMenu();            
         }      
         
-        public static void ChooseProduct()
-        {
-            int menuCount = 0;
-            foreach (Product p in Product.products)
+        public static int ChooseProduct()
+        { while (true)
             {
-                menuCount++;
-                Console.WriteLine($"{menuCount}\t{p}");
-            }
-
-            bool RuningProgram = true;
-            while (RuningProgram)
-            {
-                //prompts user to purchase a service or item.
-                Console.WriteLine();
-                Console.Write("Pick a menu item: ");                
-
-                int userpick = Validator.ValidNumAndConvertToWholeNum();
-
-                // if user does not choose 1 or 2, it will bounce back to
-
-                if (userpick < 1 || userpick > Product.products.Count)
+                int menuCount = 0;
+                foreach (Product p in Product.products)
                 {
-                    Console.WriteLine($"Invalid entry. Enter a number between 1 and {Product.products.Count}");
-                    continue;
+                    menuCount++;
+                    Console.WriteLine($"{menuCount}\t{p}");
                 }
-                else
+
+                bool RuningProgram = true;
+                while (RuningProgram)
                 {
-                    Console.WriteLine("It works!");
-                    RuningProgram = false;
+                    //prompts user to purchase a service or item.
+                    Console.WriteLine();
+                    Console.Write("Pick a menu item: ");
+
+                    int userpick = Validator.ValidNumAndConvertToWholeNum();
+
+                    // if user does not choose 1 or 2, it will bounce back to
+
+                    if (userpick < 1 || userpick > Product.products.Count)
+                    {
+                        Console.WriteLine($"Invalid entry. Enter a number between 1 and {Product.products.Count}");
+                        continue;
+                    }
+                    else
+                    {
+                        return userpick;                        
+                    }
                 }
             }
         }
@@ -57,33 +57,8 @@ namespace MidtermPOS
             {
                 Console.WriteLine(p);
             }
-        }
-
-        public static void ServiceMenu()
-        {
-            foreach (Product p in Product.products)
-                if (p.Category.ToLower() == "service")
-                {
-                    Console.WriteLine(p);
-                }
-        }
-
-        public static void ItemMenu()
-        {
-            foreach (Product p in Product.products)
-                if (p.Category.ToLower() == "item")
-                {
-                    Console.WriteLine(p);
-                }
-        }
-
-        public static void ShowInventory()
-        {
-            foreach (Product p in Product.products)
-            {
-                Console.WriteLine($"{p.Name}\t{p.Category}\t{p.Price}\t{p.Description}");
-            }
-        }
+        }        
+        
     }
 }
 
