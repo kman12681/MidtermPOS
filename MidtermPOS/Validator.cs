533f99af3a29c1aab7dd097bb1eeac820d0c7ab2
kman12681 <keith.rigling@gmail.com>
2018-05-10 15:58:15 -0400

    validator

--- MidtermPOS/Validator.cs
+++ MidtermPOS/Validator.cs
@@ -3,7 +3,6 @@ namespace MidtermPOS
 {
 	public class Validator
 	{
-
         // validate number of services or items chosen, will be used for menu selection as well
 
         public static int ValidNumAndConvertToWholeNum()
