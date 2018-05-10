3a2a90b6cc681f7977b6751d994d2eb74c83c113
kman12681 <keith.rigling@gmail.com>
2018-05-10 12:01:08 -0400

    first commit

---
+++ MidtermPOS/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermPOS
+{
+    class Product
+    {
+        private string name;
+        private string category;
+        private string description;
+        private double price;
+
+        public Product(string name, string category, string description, double price)
+        {
+            this.name = name;
+            this.category = category;
+            this.description = description;
+            this.price = price;
+        }
+    }
+}
